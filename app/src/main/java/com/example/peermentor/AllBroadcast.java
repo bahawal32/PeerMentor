a8fcfe5077d3dc9273ffc0d5ea1b251e4a502f5e
bahawal <bahawal30@gmail.com>
2019-05-09 00:00:05 +0500

    dosra try

--- app/src/main/java/com/example/peermentor/AllBroadcast.java
+++ app/src/main/java/com/example/peermentor/AllBroadcast.java
@@ -5,7 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class AllBroadcast extends AppCompatActivity {
     private LinearLayout myL;
@@ -13,6 +17,9 @@ public class AllBroadcast extends AppCompatActivity {
     dymanicViews dmy;
     Context cxt;
     int i=0;
+    String type;
+    RadioGroup typeofbroadcast;
+    RadioButton selected;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,8 +30,24 @@ public class AllBroadcast extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                     dmy = new dymanicViews(cxt);
-                    myL.addView(dmy.descriptionTextView(getApplicationContext()," "+(i++)));
+                 typeofbroadcast = findViewById(R.id.RGroup);
+                typeofbroadcast.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(RadioGroup group, int checkedId) {
+                        myL.addView(dmy.descriptionTextView(getApplicationContext()," "+(i++),type));
+                    }
+                });
+
             }
         });
+
+    }
+    void CheckButtton(View view)
+    {
+        int radId = typeofbroadcast.getCheckedRadioButtonId();
+        selected = findViewById(radId);
+        type = selected.getText().toString();
+        Toast.makeText(this,type+" was selected",Toast.LENGTH_SHORT).show();
+
     }
 }
--- app/src/main/java/com/example/peermentor/dymanicViews.java
+++ app/src/main/java/com/example/peermentor/dymanicViews.java
@@ -12,15 +12,30 @@ Context cnt;
     public dymanicViews(Context cnt) {
         this.cnt = cnt;
     }
-    public TextView descriptionTextView(Context context,String text ){
+    public TextView descriptionTextView(Context context,String text,String type ){
         final ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT);
         final TextView textView = new TextView(context);
         textView.setLayoutParams(layoutParams);
         textView.setTextSize(20);
+        textView.setHeight(30);
         textView.setTextColor(Color.rgb(0,0,0));
         textView.setText("Dynamically added text view " + text);
         textView.setMaxEms(8);
+        switch (type)
+        {
+            case "Tutor":
+                textView.setBackgroundResource(R.drawable.tutor);
+                break;
+            case "carpool":
+                textView.setBackgroundResource(R.drawable.carpooling);
+                break;
+            case "stationary":
+                textView.setBackgroundResource(R.drawable.stationary);
+                break;
+                default:
+                    textView.setBackgroundResource(R.drawable.logo);
+        }
         return textView;
     }
 
