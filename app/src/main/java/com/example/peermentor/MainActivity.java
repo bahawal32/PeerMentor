e2111f3d5e77e12576037afc077802a874104462
bahawal <bahawal30@gmail.com>
2019-05-12 05:49:42 +0500

    installed socket.io but haven't test it yet Raza making server :/

--- app/src/main/java/com/example/peermentor/MainActivity.java
+++ app/src/main/java/com/example/peermentor/MainActivity.java
@@ -1,29 +1,46 @@
 package com.example.peermentor;
 
-import android.app.ActionBar;
+
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ScrollView;
 import android.widget.TextView;
+import com.github.nkzawa.emitter.Emitter;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+import java.net.URISyntaxException;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private Socket socket;
+    {
+        try {
+           socket = IO.socket("URL Apna");
+        } catch (URISyntaxException e) {}
+    }
+
+    final Button send = findViewById(R.id.send);
+    final EditText mInputMessageView =  findViewById(R.id.msg);
+    final TextView msgBox =  findViewById(R.id.messageBox);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button send = findViewById(R.id.send);
-         final EditText messsage =  findViewById(R.id.msg);
-         final TextView msgBox =  findViewById(R.id.messageBox);
+        socket.connect();
+        socket.on("new message", onNewMessage);
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                msgBox.append("\n"+messsage.getText());
-                messsage.setText("");
+                sendMessage();
             }
         });
         Button logout = findViewById(R.id.logout);
@@ -34,5 +51,51 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
+
+    public void sendMessage(){
+        String message = mInputMessageView.getText().toString().trim();
+        if (TextUtils.isEmpty(message)) {
+            return;
+        }
+
+        mInputMessageView.setText("");
+        socket.emit("new message", message);
+    }
+
+    private Emitter.Listener onNewMessage = new Emitter.Listener() {
+        @Override
+        public void call(final Object... args) {
+
+            runOnUiThread(new Runnable() {
+
+                @Override
+                public void run() {
+                    JSONObject data = (JSONObject) args[0];
+                    String username;
+                    String message;
+                    try {
+                        username = data.getString("username");
+                        message = data.getString("message");
+                    } catch (JSONException e) {
+                        return;
+                    }
+
+                    // add the message to view
+                    addMessage(username, message);
+                }
+            });
+        }
+    };
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        socket.disconnect();
+    }
+    void addMessage(String username, String message)
+    {
+        msgBox.append(username+":"+message+"\n");
+    }
+
 }
